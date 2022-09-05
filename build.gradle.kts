98bdcfed48d04472c56d8441733b5635e6faee03
kadenscott <github.com@kaden.sh>
2022-09-05 02:23:59 -0400

    build: add dependencies, remove unused buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,31 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
     paperDevBundle("1.19.2-R0.1-SNAPSHOT")
+    implementation(libs.bedrock.core)
+    implementation(libs.cloud.paper)
+}
+
+tasks {
+    assemble {
+        dependsOn(reobfJar)
+    }
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(18)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+        expand(project.properties)
+    }
+    reobfJar {
+        outputJar.set(rootProject.layout.buildDirectory.file("libs/${project.name}.jar"))
+    }
 }
